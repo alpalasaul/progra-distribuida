34cebe085dab7bd0c2a83c41d969bc537a8a9eaa
alpalasaul <alpalasaul@gmail.com>
2022-06-16 07:51:12 -0500

    Functional weld

--- distribuida03/src/main/java/com/programacion/Principal.java
+++ distribuida03/src/main/java/com/programacion/Principal.java
@@ -1,21 +1,25 @@
 package com.programacion;
 
 import com.programacion.servicios.HelloService;
-
-import javax.enterprise.inject.Instance;
-import javax.enterprise.inject.se.SeContainer;
-import javax.enterprise.inject.se.SeContainerInitializer;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
 
 public class Principal {
 
 
     public static void main(String[] args) {
 
-        SeContainer contenedor = SeContainerInitializer.newInstance().initialize();
-        Instance<HelloService> service = contenedor.select(HelloService.class);
+//        SeContainer contenedor = SeContainerInitializer.newInstance().initialize();
+//        Instance<HelloService> service = contenedor.select(HelloService.class);
+//
+//        HelloService hs = service.get();
+//        hs.rest();
 
-        HelloService hs = service.get();
-        hs.rest();
+
+        WeldContainer weldContainer = new Weld().initialize();
+        HelloService service = weldContainer.instance().select(HelloService.class).get();
+        service.rest();
+
 
     }
 
